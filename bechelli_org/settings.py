810b826a883cc5d4e37a66cd196f5c49136dbcaf
Rodrigo Prior <rprior@infranology.com.br>
2013-01-31 23:44:43 -0200

    Update bechelli_org/settings.py
    added cms cache prefix

--- bechelli_org/settings.py
+++ bechelli_org/settings.py
@@ -34,6 +34,8 @@ CACHES = {
     }
 }
 
+CMS_CACHE_PREFIX = 'bechelliorg'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
